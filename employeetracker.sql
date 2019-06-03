2ac21c7d6eba6b93da8109f8bee796d9f21a0ee9
varunkaka <42925637+varunkaka@users.noreply.github.com>
2019-06-03 23:02:56 +1000

    updated table

--- employeetracker.sql
+++ employeetracker.sql
@@ -2,10 +2,10 @@
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 14, 2019 at 06:08 PM
--- Server version: 10.1.39-MariaDB
--- PHP Version: 7.3.5
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 03, 2019 at 01:02 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,10 +28,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `department`
 --
 
-CREATE TABLE `department` (
-  `departmentID` int(8) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE IF NOT EXISTS `department` (
+  `departmentID` int(8) UNSIGNED NOT NULL AUTO_INCREMENT,
   `departmentName` varchar(50) NOT NULL,
-  `teamLeaderID` int(8) UNSIGNED NOT NULL
+  `teamLeaderID` int(8) UNSIGNED NOT NULL,
+  PRIMARY KEY (`departmentID`),
+  KEY `empId_tlId` (`teamLeaderID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -40,8 +43,9 @@ CREATE TABLE `department` (
 -- Table structure for table `employee`
 --
 
-CREATE TABLE `employee` (
-  `empID` int(8) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE IF NOT EXISTS `employee` (
+  `empID` int(8) UNSIGNED NOT NULL AUTO_INCREMENT,
   `firstName` varchar(30) NOT NULL,
   `lastName` varchar(30) NOT NULL,
   `userName` varchar(30) NOT NULL,
@@ -61,7 +65,10 @@ CREATE TABLE `employee` (
   `emergencyContactName2` varchar(50) DEFAULT NULL,
   `emergencyContactNo2` int(11) DEFAULT NULL,
   `departmentID` int(8) UNSIGNED NOT NULL,
-  `teamLeaderID` int(8) UNSIGNED DEFAULT NULL
+  `teamLeaderID` int(8) UNSIGNED DEFAULT NULL,
+  PRIMARY KEY (`empID`),
+  UNIQUE KEY `username` (`userName`),
+  KEY `dId_employee_department` (`departmentID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -70,8 +77,9 @@ CREATE TABLE `employee` (
 -- Table structure for table `sitedetail`
 --
 
-CREATE TABLE `sitedetail` (
-  `siteID` int(8) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `sitedetail`;
+CREATE TABLE IF NOT EXISTS `sitedetail` (
+  `siteID` int(8) UNSIGNED NOT NULL AUTO_INCREMENT,
   `siteName` varchar(100) NOT NULL,
   `siteSuburb` varchar(255) NOT NULL,
   `siteCity` varchar(30) NOT NULL,
@@ -80,7 +88,8 @@ CREATE TABLE `sitedetail` (
   `siteLatitude` varchar(50) NOT NULL,
   `siteContactPersonName` varchar(30) NOT NULL,
   `siteContactPersonNo` int(11) UNSIGNED NOT NULL,
-  `siteCityPostcode` int(11) UNSIGNED NOT NULL
+  `siteCityPostcode` int(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`siteID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -89,7 +98,8 @@ CREATE TABLE `sitedetail` (
 -- Table structure for table `task`
 --
 
-CREATE TABLE `task` (
+DROP TABLE IF EXISTS `task`;
+CREATE TABLE IF NOT EXISTS `task` (
   `taskID` int(8) UNSIGNED NOT NULL,
   `taskName` varchar(50) NOT NULL,
   `taskDetail` varchar(255) NOT NULL,
@@ -98,62 +108,12 @@ CREATE TABLE `task` (
   `completionFlag` int(1) NOT NULL DEFAULT '0',
   `taskCreated` datetime NOT NULL,
   `taskCompleted` datetime DEFAULT NULL,
-  `siteID` int(8) UNSIGNED NOT NULL
+  `taskStarted` datetime NOT NULL,
+  `siteID` int(8) UNSIGNED NOT NULL,
+  PRIMARY KEY (`taskID`),
+  KEY `siteId_task_sitedetail` (`siteID`),
+  KEY `empID_emp_task` (`empID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `department`
---
-ALTER TABLE `department`
-  ADD PRIMARY KEY (`departmentID`);
-
---
--- Indexes for table `employee`
---
-ALTER TABLE `employee`
-  ADD PRIMARY KEY (`empID`),
-  ADD UNIQUE KEY `username` (`userName`),
-  ADD KEY `dId_employee_department` (`departmentID`);
-
---
--- Indexes for table `sitedetail`
---
-ALTER TABLE `sitedetail`
-  ADD PRIMARY KEY (`siteID`);
-
---
--- Indexes for table `task`
---
-ALTER TABLE `task`
-  ADD PRIMARY KEY (`taskID`),
-  ADD KEY `siteId_task_sitedetail` (`siteID`),
-  ADD KEY `empID_emp_task` (`empID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `department`
---
-ALTER TABLE `department`
-  MODIFY `departmentID` int(8) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `employee`
---
-ALTER TABLE `employee`
-  MODIFY `empID` int(8) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `sitedetail`
---
-ALTER TABLE `sitedetail`
-  MODIFY `siteID` int(8) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -163,29 +123,7 @@ ALTER TABLE `sitedetail`
 -- Constraints for table `department`
 --
 ALTER TABLE `department`
-  ADD CONSTRAINT `empId_tlId` FOREIGN KEY (`teamLeaderID`) REFERENCES `employee` (`empId`);
-
---
--- Constraints for table `employee`
---
-ALTER TABLE `employee`
-  ADD CONSTRAINT `dId_employee_department` FOREIGN KEY (`departmentID`) REFERENCES `department` (`departmentID`),
-  ADD CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`empID`) REFERENCES `task` (`empID`),
-  ADD CONSTRAINT `tlId_employee_teamLeader` FOREIGN KEY (`teamLeaderID`) REFERENCES `teamleader` (`teamLeaderID`);
-
---
--- Constraints for table `sitedetail`
---
-ALTER TABLE `sitedetail`
-  ADD CONSTRAINT `sitedetail_ibfk_1` FOREIGN KEY (`siteID`) REFERENCES `task` (`siteID`);
-
---
--- Constraints for table `task`
---
-ALTER TABLE `task`
-  ADD CONSTRAINT `empID_emp_task` FOREIGN KEY (`empID`) REFERENCES `employee` (`empId`),
-  ADD CONSTRAINT `siteId_task_sitedetail` FOREIGN KEY (`siteID`) REFERENCES `sitedetail` (`siteId`),
-  ADD CONSTRAINT `teamLeaderID_task_teamLeader` FOREIGN KEY (`teamLeaderID`) REFERENCES `teamleader` (`teamLeaderID`);
+  ADD CONSTRAINT `empId_tlId` FOREIGN KEY (`teamLeaderID`) REFERENCES `employee` (`empID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
