76bcf3827db4ff0604e8e6e195c679974f7932f8
bhavikshah746 <bhavikshah746@yahoo.com>
2019-06-03 21:35:11 +1000

    Change for operation selection
    Change in how operation is selected for different tasks.

--- Website/AppOperation.php
+++ Website/AppOperation.php
@@ -7,23 +7,39 @@ $responseArr = array();
 
 if($_SERVER['REQUEST_METHOD']=='POST'){
 	
-	if(isset($_POST['username']) && isset($_POST['password']) && $_POST["operation"]){
+	$db = new DBFunction();
+	
+	if($_POST["ActionType"] && $_POST["ActionType"]=="checkLogIn"){
 		
-		$db = new DBFunction();
-		
-		$userArr["username"] = $_POST['username'];
-		$userArr["password"] = $_POST['password'];
-		
-		if($db->validateUser($userArr)){
-			$responseArr["error"]=false;
-			$responseArr["Msg"]="Valid User";
-		}else{
-			$responseArr["error"]=true;
-			$responseArr["Msg"]="Issue while validating user. Please contact administrator";
+		if(isset($_POST['username']) && isset($_POST['password']) ){
+			
+
+			$userArr["username"] = $_POST['username'];
+			$userArr["password"] = $_POST['password'];
+
+			if($db->validateUser($userArr)){
+				$responseArr["error"]=true;
+				$responseArr["Msg"]="Valid User";
+			}else{
+				$responseArr["error"]=false;
+				$responseArr["Msg"]="Issue while validating user. Please contact administrator";
+			}
 		}
 		
-	}else{
-		$responseArr["error"]=true;
+	}else if($_POST["ActionType"] && $_POST["ActionType"]=="getTask"){
+		if(isset($_POST["username"]) && $_POST["username"]!=""){
+			$username = $_POST["username"];
+			if($db->validateUser($userArr)){
+				$responseArr["error"]=true;
+				$responseArr["Msg"]="Valid User";
+			}else{
+				$responseArr["error"]=false;
+				$responseArr["Msg"]="Issue while validating user. Please contact administrator";
+			}
+		}
+	}
+	else{
+		$responseArr["error"]=false;
 		$responseArr["Msg"]="Invalid parameters given.";
 		
 	}
