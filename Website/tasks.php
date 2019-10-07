b790b80ac30273e4e3fca05f25bb520670de4a1d
Suraj Shah <surajyogesh.shah@my.jcu.edu.au>
2019-10-07 19:39:23 +1000

    Task Added

--- Website/tasks.php
+++ Website/tasks.php
@@ -42,22 +42,7 @@ if (session_status() == PHP_SESSION_NONE) {
           <div class="content-wrapper">
             <div class="page-header">
               <h3 class="page-title"> Task Details</h3>
-              
-              <div>
-                <div class="form-check">
-                  <label class="form-check-label">
-                  <input type="radio" class="form-check-input" name="taskStatus" id="onGoing" value="ongoing" onclick="loadTable(this.value)" checked> On going </label>
-                  </div>
-                </div>
-                
-                <div class="col-sm-5">
-                  <div class="form-check">
-                    <label class="form-check-label">
-                      <input type="radio" class="form-check-input" name="taskStatus" id="completed" value="completed" onclick="loadTable(this.value)"> Completed </label>
-                  </div>
-                </div>
-                
-                <button type="button" class="btn btn-info" onClick = "location.href = 'addtask.php'">Add New Task</button>
+              <button type="button" class="btn btn-info">Add New Task</button>
             </div>
 
             <div class="row">
@@ -65,17 +50,108 @@ if (session_status() == PHP_SESSION_NONE) {
                 <div class="card">
                   <div class="card-body">
 
-                    <table class="table table-striped" id= "taskTbl">
+                    <table class="table table-striped">
                       <thead>
                         <tr>
-                          <th> Task Name </th>
-                          <th> Task Assigned To </th>
-                          <th> Task Assigned By </th>
-                          <th> Task Created On </th>
-                          <th> Task Started On </th>
+                          <th> User </th>
+                          <th> First name </th>
+                          <th> Progress </th>
+                          <th> Amount </th>
+                          <th> Deadline </th>
                         </tr>
                       </thead>
                       <tbody>
+                        <tr>
+                          <td class="py-1">
+                            <img src="../../assets/images/faces-clipart/pic-1.png" alt="image" />
+                          </td>
+                          <td> Herman Beck </td>
+                          <td>
+                            <div class="progress">
+                              <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                          </td>
+                          <td> $ 77.99 </td>
+                          <td> May 15, 2015 </td>
+                        </tr>
+                        <tr>
+                          <td class="py-1">
+                            <img src="../../assets/images/faces-clipart/pic-2.png" alt="image" />
+                          </td>
+                          <td> Messsy Adam </td>
+                          <td>
+                            <div class="progress">
+                              <div class="progress-bar bg-danger" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                          </td>
+                          <td> $245.30 </td>
+                          <td> July 1, 2015 </td>
+                        </tr>
+                        <tr>
+                          <td class="py-1">
+                            <img src="../../assets/images/faces-clipart/pic-3.png" alt="image" />
+                          </td>
+                          <td> John Richards </td>
+                          <td>
+                            <div class="progress">
+                              <div class="progress-bar bg-warning" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                          </td>
+                          <td> $138.00 </td>
+                          <td> Apr 12, 2015 </td>
+                        </tr>
+                        <tr>
+                          <td class="py-1">
+                            <img src="../../assets/images/faces-clipart/pic-4.png" alt="image" />
+                          </td>
+                          <td> Peter Meggik </td>
+                          <td>
+                            <div class="progress">
+                              <div class="progress-bar bg-primary" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                          </td>
+                          <td> $ 77.99 </td>
+                          <td> May 15, 2015 </td>
+                        </tr>
+                        <tr>
+                          <td class="py-1">
+                            <img src="../../assets/images/faces-clipart/pic-1.png" alt="image" />
+                          </td>
+                          <td> Edward </td>
+                          <td>
+                            <div class="progress">
+                              <div class="progress-bar bg-danger" role="progressbar" style="width: 35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                          </td>
+                          <td> $ 160.25 </td>
+                          <td> May 03, 2015 </td>
+                        </tr>
+                        <tr>
+                          <td class="py-1">
+                            <img src="../../assets/images/faces-clipart/pic-2.png" alt="image" />
+                          </td>
+                          <td> John Doe </td>
+                          <td>
+                            <div class="progress">
+                              <div class="progress-bar bg-info" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                          </td>
+                          <td> $ 123.21 </td>
+                          <td> April 05, 2015 </td>
+                        </tr>
+                        <tr>
+                          <td class="py-1">
+                            <img src="../../assets/images/faces-clipart/pic-3.png" alt="image" />
+                          </td>
+                          <td> Henry Tom </td>
+                          <td>
+                            <div class="progress">
+                              <div class="progress-bar bg-warning" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                          </td>
+                          <td> $ 150.00 </td>
+                          <td> June 16, 2015 </td>
+                        </tr>
                       </tbody>
                     </table>
                   </div>
@@ -84,7 +160,6 @@ if (session_status() == PHP_SESSION_NONE) {
             </div>
           </div>
           <!-- content-wrapper ends -->
-
           <!-- partial:../../partials/_footer.html -->
           <footer class="footer">
             <div class="d-sm-flex justify-content-center justify-content-sm-between">
@@ -104,7 +179,6 @@ if (session_status() == PHP_SESSION_NONE) {
     <!-- Plugin js for this page -->
     <!-- End plugin js for this page -->
     <!-- inject:js -->
-    <script src="js/task.js"></script>
     <script src="assets/js/off-canvas.js"></script>
     <script src="assets/js/hoverable-collapse.js"></script>
     <script src="assets/js/misc.js"></script>
